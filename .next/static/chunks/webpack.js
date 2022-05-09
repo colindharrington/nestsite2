becdacb198838ba3d2c16bcead9c0a9a8f6e04be
colindharrington <colindavidharrington@gmail.com>
2022-05-08 20:04:54 -0700

    latest

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "07b5088c71ab6ca8"; }
+/******/ 		__webpack_require__.h = function() { return "e68f8b933baefcde"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Newhome.js
+++ components/Newhome.js
@@ -5,6 +5,7 @@ import { useRef } from "react";
 import { useState } from "react";
 import { render } from "react-dom";
 import { motion, MotionConfig } from "framer-motion";
+import Link from 'next/link';
 
 import ReactAudioPlayer from 'react-audio-player';
 
@@ -12,12 +13,13 @@ import ReactAudioPlayer from 'react-audio-player';
 const style = {
     position: 'absolute',
     top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 600,
-    bgcolor: 'grey',
+    left: '5%',
+    // transform: 'translate(-50%, -50%)',
+    width: 400,
+    // bgcolor: 'grey',
     color:"white",
-    border: '5px solid #000',
+    // border: '2px solid #000',
+    // borderRadius: 10,
     boxShadow: 24,
     p: 4,
     textAlign:"center"
@@ -34,62 +36,83 @@ const style = {
   return (
     <div>
         <Grid container spacing={6}>
-            <Grid item xs={6}>
-            <Box sx={{textAlign:"center"}}>
-            <MotionConfig reducedMotion="user">
-                        <div className="example-container">
+            <Grid item xs={12} md={6}>
+                    <Box sx={{textAlign:"center", width:500}}>
+                        <motion.div
+                                animate={{
+                                    scale: [1, 1, 1.1, 1, 1],
+                                }}
+                                transition={{
+                                    duration: 4,
+                                    ease: "easeInOut",
+                                    times: [0, 0.2, 0.5, 0.8, 1],
+                                    repeat: Infinity,
+                                    repeatDelay: 1
+                                }}
+                                >
                             <motion.div
-                            animate={{
-                                scale: [1, 1.1, 1.1, 1, 1],
-                                rotate: [0, 90, 0, 0, 0],
-                                borderRadius: ["20%", "20%", "50%", "50%", "20%"]
-                            }}
-                            transition={{
-                                duration: 4,
-                                ease: "easeInOut",
-                                times: [0, 0.2, 0.5, 0.8, 1],
-                                repeat: Infinity,
-                                repeatDelay: 1
-                            }}
-                            />
-                        </div>
-                    </MotionConfig>
+                            className="container"
+                            whileHover={{ scale: 1.2, rotate: 10 }}
+                            whileTap={{ scale: 0.8, rotate: -10, borderRadius: "100%" }}
+                            >
+                                <Typography variant="h2" align="center">
+                                    Welcome
+                                </Typography>
+                            </motion.div>
+                        </motion.div>
+
+                        <Link href="/about">
+                            <motion.div
+                            className="container"
+                            whileHover={{ scale: 1.2, rotate: -10 }}
+                            whileTap={{ scale: 0.8, rotate: 10, borderRadius: "100%" }}
+                            >
+                                <Typography variant="h4" align="center">
+                                    click here
+                                </Typography>
+                            
+                            </motion.div>
+                        </Link>
                     </Box>
             </Grid>
-            <Grid item xs={6}>
-                 <Box sx={{textAlign:"center"}}>
-                    <motion.div
-                    className="container"
-                    whileHover={{ scale: 1.2, rotate: 10 }}
-                    whileTap={{ scale: 0.8, rotate: -10, borderRadius: "100%" }}
-                    >
-                        <Typography variant="h4" align="center">
-                            Welcome
-                        </Typography>
-                    </motion.div>
-                    <motion.div
-                    className="container"
-                    whileHover={{ scale: 1.2, rotate: -10 }}
-                    whileTap={{ scale: 0.8, rotate: 10, borderRadius: "100%" }}
-                    >
-                        <Typography variant="body1" align="center">
-                            hello
-                        </Typography>
-                    </motion.div>
-                </Box>
+            <Grid item xs={12} md={6}>
+                <Box sx={{textAlign:"center", width:500}}>
+                    <MotionConfig reducedMotion="user">
+                        <div className="example-container">
+                            <motion.div
+                                animate={{
+                                    scale: [1, 1.1, 1.1, 1, 1],
+                                }}
+                                transition={{
+                                    duration: 4,
+                                    ease: "easeInOut",
+                                    times: [0, 0.2, 0.5, 0.8, 1],
+                                    repeat: Infinity,
+                                    repeatDelay: 1
+                                }}
+                                >
+                                    <img src="/cthru.png" width="100%" alt="error" onClick={handleOpen}></img>
+                                    <Modal
+                                        open={open}
+                                        onClose={handleClose}
+                                        aria-labelledby="modal-modal-title"
+                                        aria-describedby="modal-modal-description"
+                                    >
+                                        <Box sx={style}>
+                                            <Typography id="modal-modal-title" variant="h6" component="h2">
+                                                Hexdump to find data
+                                            </Typography>
+                                            <ReactAudioPlayer
+                                                src="/final.mp3"
+                                                controls
+                                                />
+                                        </Box>
+                                    </Modal>
+                                </motion.div>
+                            </div>
+                        </MotionConfig>
+                    </Box>
             </Grid>
-            {/* <Grid item xs={12}>
-            <Box sx={{textAlign:"center", width: 300}}>
-            <motion.div
-                    className="container"
-                    whileHover={{ scale: 1.2 }}
-                    whileTap={{ scale: 0.8}}
-                    >
-            <img src="/cthru.png" width="100%" alt="error" onClick={handleOpen}></img>
-            </motion.div>
-            </Box>
-            </Grid> */}
-
         </Grid>
 
     </div>
