c333cbbfc503cd5c02b21b8886fc54faff691607
colindharrington <colindavidharrington@gmail.com>
2022-05-09 18:13:13 -0700

    misc try

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "095bae502b032488"; }
+/******/ 		__webpack_require__.h = function() { return "ab723ebd53317027"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/testing.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import { useLoader } from "@react-three/fiber";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+// function testing() {
+//   const gltf = useLoader(GLTFLoader, "/exp.glb");
+//   return (
+//     <div>
+//       <Suspense fallback={null}>
+//         <primitive object={gltf.scene} />
+//       </Suspense>
+//     </div>
+//   );
+// }
+
+// export default testing;
