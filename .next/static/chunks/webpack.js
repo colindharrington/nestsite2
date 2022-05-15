02e19eb4bf20e39d1617645a8661147ab66363a2
colindharrington <colindavidharrington@gmail.com>
2022-05-14 17:33:17 -0700

    try

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e3b40667515e2a75"; }
+/******/ 		__webpack_require__.h = function() { return "bc1b517d5ae6c2d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,25 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title> * ˚∆˙ *</title>
-        <meta name="description" content="skycube.gif" />
-        <meta property="og:image" content="skycube.gif" />
 
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="title" content="* ˚∆˙ *">
+        <meta name="description" content="skycube.gif">
+
+
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://www.aphextwins.com/">
+        <meta property="og:title" content="* ˚∆˙ *">
+        <meta property="og:description" content="skycube.gif">
+        <meta property="og:image" content="skycube.gif">
+
+
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="https://www.aphextwins.com/">
+        <meta property="twitter:title" content="* ˚∆˙ *">
+        <meta property="twitter:description" content="skycube.gif">
+        <meta property="twitter:image" content="skycube.gif">
+
+
       </Head>
       <main className={styles.main}>
         <Newhome />
