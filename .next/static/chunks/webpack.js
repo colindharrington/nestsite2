666d87100343f9505164028e8a58a05421da9842
colindharrington <colindavidharrington@gmail.com>
2022-05-14 17:34:49 -0700

    meta

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bc1b517d5ae6c2d5"; }
+/******/ 		__webpack_require__.h = function() { return "1ccce367a982a3e6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,23 @@ export default function Home() {
       <Head>
         <title> * ˚∆˙ *</title>
 
-        <meta name="title" content="* ˚∆˙ *">
-        <meta name="description" content="skycube.gif">
+        <meta name="title" content="* ˚∆˙ *"></meta>
+        <meta name="description" content="skycube.gif"></meta>
 
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:url" content="https://www.aphextwins.com/"></meta>
+        <meta property="og:title" content="* ˚∆˙ *"></meta>
+        <meta property="og:description" content="skycube.gif"></meta>
+        <meta property="og:image" content="skycube.gif"></meta>
 
-        <meta property="og:type" content="website">
-        <meta property="og:url" content="https://www.aphextwins.com/">
-        <meta property="og:title" content="* ˚∆˙ *">
-        <meta property="og:description" content="skycube.gif">
-        <meta property="og:image" content="skycube.gif">
-
-
-        <meta property="twitter:card" content="summary_large_image">
-        <meta property="twitter:url" content="https://www.aphextwins.com/">
-        <meta property="twitter:title" content="* ˚∆˙ *">
-        <meta property="twitter:description" content="skycube.gif">
-        <meta property="twitter:image" content="skycube.gif">
-
-
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta
+          property="twitter:url"
+          content="https://www.aphextwins.com/"
+        ></meta>
+        <meta property="twitter:title" content="* ˚∆˙ *"></meta>
+        <meta property="twitter:description" content="skycube.gif"></meta>
+        <meta property="twitter:image" content="skycube.gif"></meta>
       </Head>
       <main className={styles.main}>
         <Newhome />
